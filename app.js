5fa587126cb6e0956750594cbcca6d29c8764898
Detroxsys <yeudiellm@ciencias.unam.mx>
2021-07-04 13:56:49 -0500

    changes
    changes

--- app.js
+++ app.js
@@ -25,4 +25,6 @@ app.get('/home', (req, res)=> {
 }  ); 
 
 
-app.listen(3000, ()=>console.log("Servidor corriendo")); 
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT, ()=>console.log("Servidor corriendo en"+PORT)); 
